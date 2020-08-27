5690b0a04451c4ea4babae329deed455d8f61628
Ariel Rivero <ariel200506@gmail.com>
2020-08-27 01:40:46 -0300

    Cargo proyecto vacío

---
+++ src/main/java/com/clinica/modelo/ClinicaModeloApplication.java
@@ -0,0 +1,13 @@
+package com.clinica.modelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicaModeloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicaModeloApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clinica/modelo/ClinicaModeloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinica.modelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaModeloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

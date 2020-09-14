beaf5936390c8470880343a22423d8018740d344
Ariel Rivero <ariel200506@gmail.com>
2020-09-14 16:57:09 -0300

    Agrego imagenes, index y controlador de index

---
+++ src/main/java/com/clinica/modelo/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package com.clinica.modelo.controllers;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@ControllerAdvice
+public class IndexController {
+
+	
+	@RequestMapping("/")
+	public String index() {
+		
+		return "index";
+	}
+}
